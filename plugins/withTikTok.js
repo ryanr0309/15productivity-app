ebd04332aa91f90701ad6a63cd48b070628bd9a6
Ryan <ryanthony2007@gmail.com>
2026-05-02 11:43:37 -0400

    Fix TikTok framework search path via post_install hook

--- plugins/withTikTok.js
+++ plugins/withTikTok.js
@@ -11,37 +11,52 @@ function withTikTokXCFramework(config) {
       const projectRoot = config.modRequest.projectRoot;
       const platformRoot = config.modRequest.platformProjectRoot;
 
-      // Copy xcframework to ios/vendor/
+      // 1. Copy xcframework to ios/vendor/
       const src = path.join(projectRoot, 'vendor', 'TikTokBusinessSDK.xcframework');
       const destDir = path.join(platformRoot, 'vendor');
       if (!fs.existsSync(destDir)) fs.mkdirSync(destDir, { recursive: true });
       if (fs.existsSync(src)) {
         execSync(`cp -r "${src}" "${destDir}/"`);
-        console.log('[TikTokFix] xcframework copied');
+        console.log('[TikTokFix] xcframework copied to ios/vendor/');
       }
 
-      // Patch TikTokBusiness.podspec
+      // 2. Remove TikTokBusinessSDK pod dependency from podspec
       const podspecPath = path.join(
         projectRoot,
         'node_modules',
         'react-native-tiktok-business-sdk',
         'TikTokBusiness.podspec'
       );
-
       if (fs.existsSync(podspecPath)) {
         let podspec = fs.readFileSync(podspecPath, 'utf8');
         podspec = podspec.replace(/\s*s\.dependency\s+['"]TikTokBusinessSDK['"][^\n]*\n/g, '\n');
-        if (!podspec.includes('vendored_frameworks')) {
-          podspec = podspec.replace(
-            /end\s*$/,
-            `  s.vendored_frameworks = '../../ios/vendor/TikTokBusinessSDK.xcframework'
-  s.xcconfig = { 'FRAMEWORK_SEARCH_PATHS' => '"$(PODS_ROOT)/../../ios/vendor"' }
-end
-`
-          );
-        }
         fs.writeFileSync(podspecPath, podspec);
-        console.log('[TikTokFix] Patched podspec:\n', podspec);
+        console.log('[TikTokFix] Removed TikTokBusinessSDK pod dependency');
+      }
+
+      // 3. Patch the Podfile to add post_install hook
+      const podfilePath = path.join(platformRoot, 'Podfile');
+      if (fs.existsSync(podfilePath)) {
+        let podfile = fs.readFileSync(podfilePath, 'utf8');
+        const hook = `
+# TikTokFix: Add xcframework search path to TikTokBusiness pod
+post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    if target.name == 'TikTokBusiness'
+      target.build_configurations.each do |config|
+        config.build_settings['FRAMEWORK_SEARCH_PATHS'] ||= ['$(inherited)']
+        config.build_settings['FRAMEWORK_SEARCH_PATHS'] << '$(PODS_ROOT)/../../vendor'
+        config.build_settings['SWIFT_INCLUDE_PATHS'] ||= ['$(inherited)']
+        config.build_settings['SWIFT_INCLUDE_PATHS'] << '$(PODS_ROOT)/../../vendor'
+      end
+    end
+  end
+end
+`;
+        if (!podfile.includes('TikTokFix')) {
+          fs.writeFileSync(podfilePath, podfile + hook);
+          console.log('[TikTokFix] Added post_install hook to Podfile');
+        }
       }
 
       return config;
